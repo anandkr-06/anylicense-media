b82dd8c4f5f45709767cc575d379d63d0dcba804
anandkr06 <tech.anandkr06@gmail.com>
2026-06-28 19:04:28 +0530

    changes only for main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,26 +8,38 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  
+  const allowedOrigins = [
+    'http://localhost:3000',
+    'http://localhost:3200',
+    'http://localhost:3100',
+    'https://dev.anylicence.com',
+    'https://devadmin.anylicence.com',
+    'https://anylicence.com.au',
+    'https://webadmin.anylicence.com.au'
+  ];
+
   app.enableCors({
-    origin: [
-      'http://localhost:3000', // Next.js default
-      'http://localhost:3200', // Next.js default
-      'http://localhost:3100', // if Next runs on 3001
-      'https://dev.anylicence.com', // Next.js default
-      'https://devadmin.anylicence.com', //admin
-      'https://anylicence.com.au', // Next.js default
-      'https://webadmin.anylicence.com.au' //admin
-    ],
+    origin: allowedOrigins,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
 
-
   app.useStaticAssets(
     join(__dirname, '..', 'uploads'),
     {
       prefix: '/media/',
+      // Add this block to fix CORS for static files
+      setHeaders: (res, path, stat) => {
+        const origin = res.req.headers.origin;
+        if (allowedOrigins.includes(origin)) {
+          res.setHeader('Access-Control-Allow-Origin', origin);
+          res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS');
+          res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+          res.setHeader('Access-Control-Allow-Credentials', 'true');
+        }
+      }
     },
   );
 
